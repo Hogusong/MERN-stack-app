5ee96a9a2f040b400e029ade8f5174366e80cf38
“Young <hogusong1@gmail.com>
2019-12-03 20:01:54 -0500

    Implement a general Button Form component for Buttons or Link.

--- client/src/places/components/place-item.js
+++ client/src/places/components/place-item.js
@@ -1,24 +1,35 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/card";
+import Button from "../../shared/components/FormElements/button";
 
 const PlaceItem = props => {
+  const place = props.place;
+
   return (
+    <React.Fragment>
       <li className="place-item">
         <Card className="place-content">
           <div className="place-image">
-            Image of the place
+            <img src={place.imageUrl} alt={place.title} />
           </div>
           <div className="place-info">
-            Info. of the place
+            <h2>{place.title}</h2>
+            <h3>{place.address}</h3>
+            <p>{place.description}</p>
           </div>
           <div className="place-actions">
-            <button>View on Map</button>
-            <button>EDIT</button>
-            <button>DELETE</button>
+            <Button inverse>
+              View On Map
+            </Button>
+            <Button to={`/places/edit/${place.id}`} inverse>
+              EDIT
+            </Button>
+            <Button danger>DELETE</Button>
           </div>
         </Card>
       </li>
+    </React.Fragment>
   );
 };
 
