9cf42f37bf9f89942ae9e324b24d0fca01802f17
“Young <hogusong1@gmail.com>
2019-12-08 16:07:03 -0500

    create PLACES dummy-data in shared folder and user-places.js imports it.

--- client/src/places/pages/user-places.js
+++ client/src/places/pages/user-places.js
@@ -2,28 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/place-list";
-
-const DUMMY_PLACES = [
-  { 
-    id: 'p1', title: 'Empire State Building',  description: 'One of the most famous sky scrapers in the world!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/500px-Empire_State_Building_%28aerial_view%29.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: { lat: 40.7485405, lng: -73.9856584 },
-    creator: 'u1'
-  },
-  { 
-    id: 'p2', title: 'Twin Tower',  description: 'A Part of the World Trade Center in Manhattan!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/World_Trade_Center%2C_New_York_City_-_aerial_view_%28March_2001%29.jpg/800px-World_Trade_Center%2C_New_York_City_-_aerial_view_%28March_2001%29.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: { lat: 40.711077, lng: -74.014119 },
-    creator: 'u2'
-  }
-];
+import PLACES from "../../shared/dummy-data/places";
 
 const UserPlaces = () => {
   const id = useParams().id;
-
-  return <PlaceList places={DUMMY_PLACES.filter(P => P.creator === id)}/>;
-};
+  return <PlaceList places={PLACES.filter(P => P.creator === id)}/>
+}
 
 export default UserPlaces;
