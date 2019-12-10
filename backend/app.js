43a1fcbd77e6e3d2e25c1092b64410e926360bf7
“Young <hogusong1@gmail.com>
2019-12-10 01:53:25 -0500

    add an error handler and body-parser to app.js to add or update a place.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const placeRoutes = require('./routes/place-routes');
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placeRoutes);
+
+// if a wrong route is provided, this error occur.
+app.use((req, res, next) => {
+  throw new HttpError(404, 'Could not find this route.')
+});
 
 // handling all ERRORS which are sent by 'throw error' or 'return next(error)'.
 app.use((error, req, res, next) => {
